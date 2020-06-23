27cf62fc82418cb5fb90ce270ac0286c10fa3bcf
Raja Baz <raja.e.baz@gmail.com>
2020-06-23 08:12:09 +0300

    Fix: rate-limit counter shouldn't be global, it should be per-channel
    Also: don't rate-limit on private channels

--- bot.py
+++ bot.py
@@ -9,14 +9,24 @@ from util import render_rates, get_token
 from telegram.ext import Updater, CommandHandler
 import datetime
 
-last_rates_time = None
-def print_rates(update, context):
-    global last_rates_time
+last_rates_times = {}
 
+def rate_limit(chat):
+    if chat.type == 'private':
+        return False
+    
+    last_rates_time = last_rates_times[chat.id] if chat.id in last_rates_times else None
     now = datetime.datetime.now()
-    if last_rates_time is None or (now - last_rates_time).total_seconds() > 3600:
-        update.message.reply_text(render_rates())
-        last_rates_time = now
+    if last_rates_time is not None and (now - last_rates_time).total_seconds() <= 3600:
+        return True
+
+    last_rates_times[chat.id] = now
+    return False
+
+def print_rates(update, context):
+    if rate_limit(update.effective_chat):
+        return
+    update.message.reply_text(render_rates())
 
 TOKEN = get_token()
 
