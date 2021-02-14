0ebac581f0be851f6d14b4783147c22b657a051a
Raja Baz <raja.e.baz@gmail.com>
2021-02-14 21:47:12 +0200

    Get around the block from lirarate which seems to filter on useragent

--- fetchers/lirarate.sh
+++ fetchers/lirarate.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-auth=$(curl -s https://lirarate.com/wp-content/themes/lirarate/assets/js/main.js | grep "'Authorization'" | sed 's/.*Authorization'"'"'*: *//' | sed "s/'//g")
+auth=$(curl -s 'https://lirarate.com/wp-content/themes/lirarate/assets/js/main.js?ver=1.0.34'\
+            -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5'\
+            --compressed -H 'Connection: keep-alive' -H 'Referer: https://lirarate.com/'  -H 'Sec-GPC: 1' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache'\
+      | grep "'Authorization'" | sed 's/.*Authorization'"'"'*: *//' | sed "s/'//g")
+
 ver=$(date '+%Y%-m%-d%-H')
-curl -s "https://lirarate.com/wp-json/lirarate/v2/rates?currency=LBP&_ver=$ver" -H "Authorization: $auth" -H 'referer: https://lirarate.com/'| python3 "$ROOT"/parse_lira_rate.py
+curl -s "https://lirarate.com/wp-json/lirarate/v2/rates?currency=LBP&_ver=$ver" -H "Authorization: $auth" -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5'\
+             --compressed -H 'Connection: keep-alive' -H 'Referer: https://lirarate.com/'  -H 'Sec-GPC: 1' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache'\
+      | python3 "$ROOT"/parse_lira_rate.py
 
