146380c8beda8604c21c4b4d7f7796d7fa78ba8d
Raja Baz <raja.e.baz@gmail.com>
2021-12-10 16:11:05 +0400

    For consistency, call the OMT rate 'sell'
    ie, this is the rate at which one sells dollars to OMT and not the
    other way around (this is way all rates are phrased. From the POV of
    the person using the bot buying/selling USD)

--- fetchers/omt.py
+++ fetchers/omt.py
@@ -11,5 +11,5 @@ date_string = container('span')[-1].text
 
 rate = rate_string.split()[-2].replace(",", "")
 
-result = {'buy': rate, 'sell': None, 'time': date_string}
+result = {'buy': None, 'sell': rate, 'time': date_string}
 print(json.dumps(result))
