3034c3ddff4b0fc5fca2e1cc8fc5425b8fa10b7f
Raja Baz <raja.e.baz@gmail.com>
2022-07-03 10:43:50 +0400

    Fix: sarraf changed endpoint

--- fetchers/sarraf.sh
+++ fetchers/sarraf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://dola-62d9a.firebaseapp.com  2>/dev/null | grep /co/ | tail -n2 | python3 "$ROOT"/parse_sarraf.py
+curl https://dollebweb.firebaseapp.com  2>/dev/null | grep /co/ | tail -n2 | python3 "$ROOT"/parse_sarraf.py
