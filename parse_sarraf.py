859636a7cc22be5e372469ba5f23a7a3b1926861
Raja Baz <raja.e.baz@gmail.com>
2023-10-30 10:54:46 +0000

    Fix: make sarraf parser more tolerant of weird spacing bullshit

--- parse_sarraf.py
+++ parse_sarraf.py
@@ -1,9 +1,16 @@
 import json
+import re
 
 rates_line = input()
 time_line = input()
 
-rates = [int(x.replace(",", "")) for x in rates_line[4:].strip().split()]
+capture_number = "([0-9]+\s*,\s*[0-9]+)"
+regexp = re.compile(f"/co/\s*{capture_number}\s+{capture_number}")
+
+def captured_to_num(s):
+    return int(s.replace(" ", "").replace(",", ""))
+
+rates = [captured_to_num(s) for s in regexp.match(rates_line).groups()]
 time = time_line[4:].strip().split()[0]
 result = {'buy': rates[1], 'sell':rates[0], 'time': time}
 print(json.dumps(result))
